9ed10f10d9422313f8c4c735e2d2063ce147af53
Black Cat <jakoblstark@gmail.com>
2024-09-21 21:44:30 -0600

    Initial commit

---
+++ src/main/java/net/black/ushr/UshrDataGenerator.java
@@ -0,0 +1,11 @@
+package net.black.ushr;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UshrDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
